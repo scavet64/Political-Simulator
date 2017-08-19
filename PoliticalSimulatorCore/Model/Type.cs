e4c0d92964013906c39db0f913026a707f7e9060
Vincent Scavetta <scavet64@students.rowan.edu>
2017-08-19 18:05:36 -0400

    Sharping the type enum

--- PoliticalSimulatorCore/Model/Type.cs
+++ PoliticalSimulatorCore/Model/Type.cs
@@ -7,72 +7,95 @@ using System.Threading.Tasks;
 namespace PoliticalSimulatorCore.Model
 {
     /// <summary>
-    /// This is going to need full scale porting: https://stackoverflow.com/questions/469287/c-sharp-vs-java-enum-for-those-new-to-c
+    /// Type Class
     /// </summary>
-    public enum Type
+    public class Type
     {
-        Fighter ("Genius", "Charasmatic"),
-        Psycho ("Spooky", "Magic"),
-        Magic ("Psycho", "Genius"),
-        Genius ("Magic", "Fighter"),
-        Charasmatic ("Fighter", "Spooky"),
-        Spooky ("Charasmatic", "Psycho"),
-        Forbidden("", "");
-    
 
-    private final int HIGHLY_EFFECTIVE_MODIFIER = 2;
-    private final int NOT_VERY_EFFECTIVE_MODIFIER = -2;
-    private final String HIGHLY_EFFECTIVE_STRING = "Attack was highly effective!\n";
-	private final String NOT_VERY_EFFECTIVE_STRING = "Attack was not very effective\n";
-	
-	String highlyEffective;
-    String notVeryEffective;
+        private const int HIGHLY_EFFECTIVE_MODIFIER = 2;
+        private const int NOT_VERY_EFFECTIVE_MODIFIER = -2;
+        private const String HIGHLY_EFFECTIVE_STRING = "Attack was highly effective!\n";
+        private const String NOT_VERY_EFFECTIVE_STRING = "Attack was not very effective\n";
+
+        protected enum TypeEnum { Fighter, Psycho, Magic, Genius, Charasmatic, Spooky, Forbidden, none }
+        public readonly static Type FIGHTER = new Type(TypeEnum.Fighter, TypeEnum.Genius, TypeEnum.Charasmatic);
+        public readonly static Type PSYCHO = new Type(TypeEnum.Psycho, TypeEnum.Spooky, TypeEnum.Magic);
+        public readonly static Type MAGIC = new Type(TypeEnum.Magic, TypeEnum.Psycho, TypeEnum.Genius);
+        public readonly static Type GENIUS = new Type(TypeEnum.Genius, TypeEnum.Magic, TypeEnum.Fighter);
+        public readonly static Type CHARASMATIC = new Type(TypeEnum.Charasmatic, TypeEnum.Fighter, TypeEnum.Psycho);
+        public readonly static Type SPOOKY = new Type(TypeEnum.Spooky, TypeEnum.Charasmatic, TypeEnum.Psycho);
+        public readonly static Type FORBIDDEN = new Type(TypeEnum.Forbidden, TypeEnum.none, TypeEnum.none);
+
+        private TypeEnum highlyEffective;
+        private TypeEnum notVeryEffective;
+        protected TypeEnum Self { get; private set; }
 
 
-    Type(String highlyEffective, String notVeryEffective)
-    {
-        this.highlyEffective = highlyEffective;
-        this.notVeryEffective = notVeryEffective;
-    }
+        private Type(TypeEnum self, TypeEnum highlyEffective, TypeEnum notVeryEffective)
+        {
+            this.Self = self;
+            this.highlyEffective = highlyEffective;
+            this.notVeryEffective = notVeryEffective;
+        }
 
-    /**
-	 * @param typeToCompare type to check for changes from other types
-	 * @return int value for attack change due to types
-	 */
-    public int applyModifier(Type typeToCompare)
-    {
-        String opposingType = typeToCompare.toString();
-        if (opposingType.equals(highlyEffective))
+        /**
+         * @param typeToCompare type to check for changes from other types
+         * @return int value for attack change due to types
+         */
+        public int applyModifier(Type typeToCompare)
         {
-            return HIGHLY_EFFECTIVE_MODIFIER;
+            TypeEnum opposingType = typeToCompare.Self;
+            if (opposingType.Equals(highlyEffective))
+            {
+                return HIGHLY_EFFECTIVE_MODIFIER;
+            }
+            else if (opposingType.Equals(notVeryEffective) || opposingType.Equals(this.ToString()))
+            {
+                return NOT_VERY_EFFECTIVE_MODIFIER;
+            }
+            else
+            {
+                return 0;
+            }
         }
-        else if (opposingType.equals(notVeryEffective) || opposingType.equals(this.toString()))
-        {
-            return NOT_VERY_EFFECTIVE_MODIFIER;
-        }
-        else
-        {
-            return 0;
-        }
-    }
 
-    /**
-	 * @param typeToCompare Type to be checked for bonus effects on damage exchange
-	 */
-    public String modifierString(Type typeToCompare)
-    {
-        String opposingType = typeToCompare.toString();
-        if (opposingType.equals(highlyEffective))
+        /**
+         * @param typeToCompare Type to be checked for bonus effects on damage exchange
+         */
+        public String modifierString(Type typeToCompare)
         {
-            return HIGHLY_EFFECTIVE_STRING;
+            String opposingType = typeToCompare.ToString();
+            if (opposingType.Equals(highlyEffective))
+            {
+                return HIGHLY_EFFECTIVE_STRING;
+            }
+            else if (opposingType.Equals(notVeryEffective) || opposingType.Equals(this.ToString()))
+            {
+                return NOT_VERY_EFFECTIVE_STRING;
+            }
+            else
+            {
+                return "";
+            }
         }
-        else if (opposingType.equals(notVeryEffective) || opposingType.equals(this.toString()))
+
+        // override object.Equals
+        public override bool Equals(object obj)
         {
-            return NOT_VERY_EFFECTIVE_STRING;
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Type compareType = (Type)obj;
+
+            return compareType.Self == Self;
         }
-        else
+
+        // override object.GetHashCode
+        public override int GetHashCode()
         {
-            return "";
+            return Self.GetHashCode();
         }
     }
 }
